e6d0baba3b2cf2323bc2b6dd917cbcf4ccf4aa66
00017298 <00017298@wiut.uz>
2024-12-05 20:50:30 +0500

    debugging

---
+++ 00017298_API/README.md
@@ -0,0 +1,2 @@
+# WAD_CW_00017298
+This application was developed for Web Application module, as coursework portfolio project @ WIUT by student ID: 00017298
--- 00017298_API/Repositories/IRepository_00017298.cs
+++ 00017298_API/Repositories/IRepository_00017298.cs
@@ -1,6 +1,11 @@
 ﻿namespace _00017298_API.Repositories
 {
-    public interface IRepository_00017298
+    public interface IRepository_00017298<T>
     {
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Delete(int id);
+        Task<T> Get(int id);
+        Task<IEnumerable<T>> GetAll();
     }
 }
